1ee504f4fa7588b0db02d3bd6931f81be94543aa
M Haidar Hanif <mhaidarhanif@gmail.com>
2013-03-16 21:12:15 +0700

    Prepare for leak resistant drawable resource

--- src/com/mhaidarhanif/android/kargu/MainActivity.java
+++ src/com/mhaidarhanif/android/kargu/MainActivity.java
@@ -2,6 +2,7 @@ package com.mhaidarhanif.android.kargu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -9,20 +10,27 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-  // For complete title purpose, will be used later
+  // For complete title purpose. Will be used later.
   // String appEditionName = getString(R.string.app_name) + " " + getString(R.string.name_edition);
 
   public final static String EXTRA_NUMBER = "com.mhaidarhanif.android.kargu.NUMBER";
+  private static Drawable sLogoKargu;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    // Avoid leaks by keep potential used image in a static field.
+    if (sLogoKargu == null) {
+      sLogoKargu = this.getResources().getDrawable(R.drawable.ic_launcher);
+    }
+
     setContentView(R.layout.activity_main);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
+    // Inflate the menu. This adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
     return true;
   }
